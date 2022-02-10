662ee3cf75dda2117abb019fe3a17ff5301741c9
Johnny Martinez <mrtnz.jn@gmail.com>
2022-02-10 08:16:29 -0600

    added while-loops-exercises.html and while-loops-exercises.js.

---
+++ js/while-loops-exercises.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//While Loop Exercise
+
+var i = 1;
+
+while (i < 65536) {
+
+        console.log(i *= 2);
+
+}
+
+
+//Do While Loop Exercise
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+Math.floor(Math.random() * 5) + 1;
+
+
+
