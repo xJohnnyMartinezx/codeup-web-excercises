4ae4d8221b6a0e09e88e15ecfa42d664cc76e204
Johnny Martinez <mrtnz.jn@gmail.com>
2022-02-13 16:30:11 -0600

    renamed while-loops-exercises.js to while.js and while-loops-exercises.html to while.html

---
+++ js/while.js
@@ -0,0 +1,64 @@
+"use strict";
+
+//While Loop Exercise
+
+var i = 1;
+
+while (i < 65536) {
+
+        console.log(i *= 2);
+
+}
+
+
+
+
+
+//Do While Loop Exercise
+
+// // This is how you get a random number between 50 and 100
+// var conesInInventory = Math.floor(Math.random() * 50) + 50;
+//
+//
+// // This expression will generate a random number between 1 and 5
+//
+// do { // each iteration is a new customer
+//     let requestedCones = Math.floor(Math.random() * 5) + 1;
+//     // do you have enough cones to sell them?
+//     // what happens when you 'sell' them a cone?
+//
+// }while()// when do you stop selling cones
+
+// This is how you get a random number between 50 and 100
+//First write enough code that generates a random number between 50 and 100
+//representing the amount of cones to sell before you start your loop.
+
+
+
+//
+// var allCones = 3
+//     //Math.floor(Math.random() * 50) + 50;
+// // This expression will generate a random number between 1 and 5
+// var conesBought = Math.floor(Math.random() * 5) + 1;
+//
+//
+// console.log(allCones);
+//
+// do { //each iteration is a new customer
+//     //number of cones being bought from allCones
+//     var conesRemaining = allCones - conesBought;
+//     if (conesBought < 6) {
+//         console.log("No problem, I can sell you " + conesBought + " from my inventory of " + allCones);
+//         console.log(conesRemaining);
+//     }else if (conesBought > conesRemaining) {
+//         console.log("Sorry I can only sell you a maximum of " + conesRemaining)
+//     }
+//
+//
+//
+//      }while (allCones === 0);
+
+
+
+
+
