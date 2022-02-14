c36ddc92e4dee589b74f68574fb7eec2df0bdc51
Johnny Martinez <mrtnz.jn@gmail.com>
2022-02-14 08:50:48 -0600

    modified external.js

--- js/external.js
+++ js/external.js
@@ -1,34 +1,34 @@
 "use strict";
 
-// console.log("Hello from external JavaScript")
+console.log("Hello from external JavaScript")
 
-// alert ("Welcome to my first Website!")
-//
-// let favColor = prompt("What is your favorite color?")
-//
-//     alert("Nice, " + favColor + " is my favorite color too!")
-//
-// var littleMermaid = prompt("How many days would you like to rent The Little Mermaid for?")
-//
-// // alert("Okay, your total for this rental is $" + littleMermaid * 3)
-//
-// var brotherBear = prompt("How many days would you like to rent Brother Bear for?")
-//
-// // alert("Okay, your total for this rental is $" + brotherBear * 3)
-//
-// var herc = prompt("How many days would you like to rent Hercules for?")
-//
-// // alert("Okay, your total for this rental is $" + herc * 3)
-//
-// alert("So your overall total for all three rentals is $" + (littleMermaid * 3 + brotherBear * 3 + herc * 3))
-//
-// let goog = prompt("How many hours did you work for Google this week?")
-//
-// let amz = prompt("How many hours did you work for Amazon this week?")
-//
-// let fb = prompt("How many hours did you work for Facebook this week?")
-//
-// alert("So your overall total pay for this week is $" + (goog * 400 + amz * 380 + fb * 350))
+alert ("Welcome to my first Website!")
+
+let favColor = prompt("What is your favorite color?")
+
+    alert("Nice, " + favColor + " is my favorite color too!")
+
+var littleMermaid = prompt("How many days would you like to rent The Little Mermaid for?")
+
+// alert("Okay, your total for this rental is $" + littleMermaid * 3)
+
+var brotherBear = prompt("How many days would you like to rent Brother Bear for?")
+
+// alert("Okay, your total for this rental is $" + brotherBear * 3)
+
+var herc = prompt("How many days would you like to rent Hercules for?")
+
+// alert("Okay, your total for this rental is $" + herc * 3)
+
+alert("So your overall total for all three rentals is $" + (littleMermaid * 3 + brotherBear * 3 + herc * 3))
+
+let goog = prompt("How many hours did you work for Google this week?")
+
+let amz = prompt("How many hours did you work for Amazon this week?")
+
+let fb = prompt("How many hours did you work for Facebook this week?")
+
+alert("So your overall total pay for this week is $" + (goog * 400 + amz * 380 + fb * 350))
 
 
 
