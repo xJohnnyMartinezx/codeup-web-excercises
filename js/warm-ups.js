a368aa15e7c2e265a37e986eec140184b265cb38
Johnny Martinez <mrtnz.jn@gmail.com>
2022-02-16 12:04:00 -0600

    added warm-ups.js

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -4,19 +4,131 @@
 // Example input: [95, 74, 86, 100]
 // Example output: 88.75 (edited)
 
-var arrOfIntegers = [95, 74, 86, 100]
-// var arrSum = arrOfIntegers.length
+// var arrOfIntegers = [95, 74, 86, 100]
+// // var arrSum = arrOfIntegers.length
+//
+//
+// function findAverage() {
+//     var sum = arrOfIntegers.reduce((a, b) => a + b, 0);
+//     console.log(sum)
+//
+//     var result = sum/arrOfIntegers.length || 0;
+//     return result;
+//
+// }
+// findAverage(arrOfIntegers)
+//
 
 
-function findAverage() {
-    var sum = arrOfIntegers.reduce((a, b) => a + b, 0);
-    console.log(sum)
+//JS Object Warmup, pt 1:
+// Create an object representing a person. It should have properties representing:
+// names (an array of strings)
+// date of birth
+// occupation
+// Hard code some values in that object, then log it and its properties to the console
 
-    var result = sum/arrOfIntegers.length || 0;
-    return result;
+
+var person;
+person = {
+    names: ["Johnny", "Martinez"],
+    dob: "01/01/1988",
+    occupation: "Student"
+}
+console.log(person);
+console.log(person.names);
+console.log(person.names[0]);
+console.log(person.names[1]);
+console.log(person.dob);
+console.log(person.occupation);
+
+person.names.forEach((name)=> {
+    console.log(name);
+})
+
+var person2 = Object.create(person);
+person2["names"] = ["Ry", "Sutton"];
+person2["occupation"] = "intructor";
+person2.dob = "02.02.1994";
+console.log(person2);
+person2["dob"] = "02.03.1994";
+console.log(person2);
+
+
+// function Person(name, occupation, dob) {
+//     this.names = names;
+//     this.occupation = occupation;
+//     this.dob = dob;
+//
+//     var fakePerson = new Person("fakename", "fakework","01.01.1999")
+//     console.log(fakePerson);
+// }
+
+//JS Object Warmup, pt 2:
+// Create an object representing a person’s contact info. It should have:
+// phone number
+// street address
+// state
+// postal code
+// email address
+// Hard code some values in that object, then log it and its properties to the console
+
+var contactInfo = {
+    phone: "555-555-5555",
+    addressNumber: "123",
+    streetName: "Fake St.",
+    city: "Dallas",
+    state: "Texas",
+    zipcode: "85449",
+    email: "fakecontact@gmail.com"
+}
+
+console.log(contactInfo);
+
+//Defines the contactInfo object
+function ContactInfo(phone,addressNumber,streetName,city,state,zipcode,email) {
+    this.phone = phone;
+    this.addressNumber = addressNumber;
+    this.streetName = streetName;
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+    this.email = email;
+
+    var newContact = new ContactInfo("123-456-7899", "456", "Fake2 St.", "Dallas", "Texas", "89466", "fake2@gmail.com")
+    console.log(newContact)
 
 }
-findAverage(arrOfIntegers)
 
 
-fu
+//////////////////////////////////////////////
+//
+// // Defines a contact info object
+// function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress){
+//     this.phoneNumber = phoneNumber;
+//     this.streetAddress = streetAddress;
+//     this.city = city;
+//     this.state = state;
+//     this.postalCode = postalCode;
+//     this.emailAddress = emailAddress;
+// }
+// // Defines a person object
+// function Person(names, occupation, dob, contactInfo) {
+//     this.names = names;
+//     this.occupation = occupation;
+//     this.dob = dob;
+//     this.contactInfo = contactInfo;
+// }
+// // makes a new person object instance (by invoking the constructor)
+// let person2 = new Person(
+//     ["Casey", "J", "Edwards"],
+//     "Cat Herder / Firefighter",
+//     "11.30.1989",
+//     new ContactInfo(
+//         '123.456.7890',
+//         '123 Anywhere Ln',
+//         'Beverly Hills',
+//         'CA',
+//         '90210',
+//         'nope@no-no-no.no'));
+//
+// console.log(person2.contactInfo.emailAddress);
