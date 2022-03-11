917177ea2e6f816c3c957ce1c232c3fcbcabf01e
Johnny Martinez <mrtnz.jn@gmail.com>
2022-03-11 15:09:18 -0600

    completed excercises for jquery intro and mouse events lessons.

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,5 +3,44 @@
 $(document).ready(function() {
 
 
+  // $(".codeup").css("border", "solid red 1px")
+
+    // $("ul>li").css("font-size", "20px")
+
+    // $("h1, p, li").css("background-color", "yellow")
+
+    // var contents = $("h1").text()
+    // alert(contents);
+
+//    -----------jQuery mouse events exercises-----------
+
+    $("h1").click(function () {
+
+        if ($(this).css("background-color") === "rgba(0, 0, 0, 0)"){
+            $(this).css("background-color", "green")}
+        else {
+            $(this).css("background-color", "rgba(0, 0, 0, 0)")
+        }
+    })
+
+    $("div>p").dblclick(function (){
+        $(this).css("font-size", "18px")
+    })
+
+    $("ul>li").hover(function() {
+            $(this).css('color', 'red');
+        },
+        function() {
+            $(this).css('color', 'black');
+        })
 
 });
+
+
+//-------------keyboard events--------------------
+
+$("#search").keyup(function (e) {
+    console.log(e.key)
+
+
+})
