a9a52656c36fd6b910dea087079dfe879b964dcf
Johnny Martinez <mrtnz.jn@gmail.com>
2022-03-22 07:29:40 -0500

    added weather_map.css and updated all other weather_map files.

--- js/mapbox_weather_api.js
+++ js/mapbox_weather_api.js
@@ -4,6 +4,9 @@
 let map;
 let marker;
 let geocoder;
+let popup;
+let city;
+
 
 initialize()
 setGeocoderEventListener()
@@ -14,12 +17,12 @@ function initialize() {
     mapboxgl.accessToken = MAP_BOX;
     map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9',
+        style: "mapbox://styles/mapbox/dark-v10",
         zoom: 10,
         center: [-96.7970, 32.7767]
     });
     getWeatherData(map.getCenter().lat, map.getCenter().lng)
-// console.log(map.getCenter().lat)
+
 
     geocoder = new MapboxGeocoder({
         accessToken: MAP_BOX,
@@ -33,36 +36,69 @@ function initialize() {
 }
 
 
-
-    marker = new mapboxgl.Marker({
-        color: "green",
-        draggable: true
-    }).setLngLat([map.getCenter().lng, map.getCenter().lat])
-        .addTo(map)
-
-
-    function onDragEnd() {
-        const lngLat = marker.getLngLat();
-        getWeatherData(lngLat.lat, lngLat.lng)
-    }
-
-    marker.on('dragend', onDragEnd);
+marker = new mapboxgl.Marker({
+    color: "green",
+    draggable: true
+}).setLngLat([map.getCenter().lng, map.getCenter().lat])
+    .addTo(map)
 
 
 
+function onDragEnd() {
+    const lngLat = marker.getLngLat();
+    getWeatherData(lngLat.lat, lngLat.lng)
+}
+
+marker.on('dragend', onDragEnd);
+
+
+// function getPopup(description, coordinates) {
+//     return new mapboxgl.Popup()
+//         // .setLngLat(coordinates)
+//         .setLngLat([map.getCenter().lng, map.getCenter().lat])
+//         .setHTML(`<p>${description}</p>`)
+//         .addTo(map);
+// }
 
 
 
-    function setGeocoderEventListener() {
-        geocoder.on("result", function (e) {
-            let searchCoordLng = e.result.geometry.coordinates[0];
-            let searchCoordLat = e.result.geometry.coordinates[1];
-            getWeatherData(searchCoordLat, searchCoordLng);
+function setGeocoderEventListener() {
+    geocoder.on("result", function (e) {
+        let searchCoordLng = e.result.geometry.coordinates[0];
+        let searchCoordLat = e.result.geometry.coordinates[1];
+        console.log(e)
+        getWeatherData(searchCoordLat, searchCoordLng);
+
+        if (marker) {
+            marker.remove();
+        }
+
+        marker(e.result.geometry.coordinates);
+        // city = currentCity(e.result.place_name);
 
 
-        });
 
-    }
+    });
+
+
+}
+
+
+//Map Menu
+const layerList = document.getElementById('menu');
+const inputs = layerList.getElementsByTagName('input');
+
+for (const input of inputs) {
+    input.onclick = (layer) => {
+        const layerId = layer.target.id;
+        map.setStyle('mapbox://styles/mapbox/' + layerId);
+    };
+}
+
+// function extractCurrentCity(city) {
+//     console.log(city)
+//
+// }
 
 
 
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,68 +1,72 @@
 "use strict";
 
 
-    function getWeatherData(lat, lon) {
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${OWN_KEY}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                $(`#weather`).html(buildCardContent(data.daily));
+function getWeatherData(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${OWN_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            $(`#weather`).html(buildCardContent(data.daily));
 
-            })
+        })
+}
+
+function extractWeatherData(dayObj) {
+    return {
+        date: dayObj.dt,
+        dailyTemp: dayObj.temp.day,
+        description: dayObj.weather[0].description,
+        humidity: dayObj.humidity,
+        wind_speed: dayObj.wind_speed,
+        pressure: dayObj.pressure
     }
+}
 
-    function extractWeatherData(dayObj) {
-        return {
-            date: dayObj.dt,
-            dailyTemp: dayObj.temp.day,
-            description: dayObj.weather[0].description,
-            humidity: dayObj.humidity,
-            wind_speed: dayObj.wind_speed,
-            pressure: dayObj.pressure
-        }
+function buildCardContent(dayArr) {
+    let html = '<div class="row">'
+    for (let i = 0; i < 5; i++) {
+        html += buildWeatherCard(dayArr[i]);
     }
-
-    function buildCardContent(dayArr) {
-        let html = '<div class="row">'
-        for (let i = 0; i < 5; i++) {
-            html += buildWeatherCard(dayArr[i]);
-        }
-        html += '</div>';
-        return html;
-    }
+    html += '</div>';
+    return html;
+}
 
 
-    function buildWeatherCard(day) {
-        let html = ""
-        let weather = extractWeatherData(day);
-        let formattedDate = formatDate(weather.date);
-        //language=HTML
-        html += `
-            <div class="card-header">
+function buildWeatherCard(day) {
+    let html = ""
+    let weather = extractWeatherData(day);
+    let formattedDate = formatDate(weather.date);
+
+    let format = {
+        day: "numeric",
+        month: "2-digit",
+        year: "numeric"
+    };
+
+    // console.log(weather.date.toLocaleString("en-US", format))
+    //language=HTML
+    html += `
+      
+        <section class="col-12 col-sm-6 col-lg-4 col-xl-4 col-xxl-2 mx-auto mt-2">
+            <div class="card-header mx-auto">
                 ${formattedDate}
             </div>
-            <div id="five-day-forecast" class="card">
-                <p>Today's Temperature: ${weather.dailyTemp}</p>
-                <p>Description: ${weather.description}</p>
-                <p>Humidity: ${weather.humidity}</p>
-                <p>Wind speed is : ${weather.wind_speed}</p>
-                <p>Pressure: ${weather.pressure}</p>
+            <div id="five-day-forecast" class="card border-5 mx-0">
+                <p>Average Daytime Temperature: <b>${weather.dailyTemp} °F</b> </p>
+                <p>Description: <b>${weather.description}</b></p>
+                <p>Humidity: <b>${weather.humidity}%</b></p>
+                <p>Wind speed is : <b>${weather.wind_speed} MPH</b></p>
+                <p>Pressure: <b>${weather.pressure} hPa</b></p>
 
-            </div>`
-        return html
+            </div>
+        </section>`
+    return html
 
 
-    }
+}
 
-    function formatDate(unixDate) {
-        return new Date(unixDate * 1000).toISOString().split("T")[0];
-    }
-
-    // $('#submit').click(function (e) {
-    //     e.preventDefault();
-    //     let userLat = $('#lat').val();
-    //     let userLon = $('#lon').val();
-    //     getWeatherData(userLat, userLon);
-    // })
+function formatDate(unixDate) {
+    return new Date(unixDate * 1000).toISOString().split("T")[0];
+}
 
 
